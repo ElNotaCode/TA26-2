6ce2591ba896135444c1c9773e5f59990253de3f
Eloi Martorell Martin <eloi.martin174@gmail.com>
2022-05-18 14:14:00 +0200

    a

--- TA26-2/src/main/java/com/crud/spring/controller/ProyectosController.java
+++ TA26-2/src/main/java/com/crud/spring/controller/ProyectosController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,7 +37,7 @@ public class ProyectosController {
 		return proyectoServiceImpl.readProyecto(id);
 	}
 	
-	@GetMapping("/proyectos/{id}")
+	@PutMapping("/proyectos/{id}")
 	public Proyecto updateProyecto(@RequestBody Proyecto proyecto,@PathVariable(name="id") String id) {
 		
 		Proyecto proyecto_seleccionado = new Proyecto();
